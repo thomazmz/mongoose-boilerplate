d564a6c45dd7909411737e5a00d96a47b885d743
Thomaz <thomaz.zandonotto@gmail.com>
2019-09-17 13:13:22 -0300

    Add default 404 route

--- app/index.js
+++ app/index.js
@@ -13,4 +13,10 @@ application.use(bodyParser.json());
 application.use("/user", userRoutes);
 application.use("/todo", todoRoutes);
 
+app.use((res, req, next) => { 
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
 module.exports = application;
